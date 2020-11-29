af9759c8c0fe6c7c89d16adecccdc8d6e5edaf26
Greg Jett <greg.jett25@gmail.com>
2020-11-29 17:12:36 -0600

    added body

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-$(document).ready(function() {
-  $(".menu-icon").on("click", function() {
-        $("nav ul").toggleClass("showing");
+// $(document).ready(function() {
+//   $(".menu-icon").on("click", function() {
+//         $("nav ul").toggleClass("showing");
+//   });
+// });
+
+// // Scrolling Effect
+
+// $(window).on("scroll", function() {
+//   if($(window).scrollTop()) {
+//         $('nav').addClass('black');
+//   }
+
+//   else {
+//         $('nav').removeClass('black');
+//   }
+// })
+
+$(function () {
+      $(window).on('scroll', function () {
+          if ( $(window).scrollTop() > 5 ) {
+              $('.navbar').addClass('active');
+          } else {
+              $('.navbar').removeClass('active');
+          }
+      });
   });
-});
 
-// Scrolling Effect
-
-$(window).on("scroll", function() {
-  if($(window).scrollTop()) {
-        $('nav').addClass('black');
-  }
-
-  else {
-        $('nav').removeClass('black');
-  }
-})
